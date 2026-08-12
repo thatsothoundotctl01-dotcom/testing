abf76b01a65a85a3c3c55019aa8945b46c444b7d
thatsothoun4-dotcom <thatsothoun4@gmail.com>
2026-08-12 17:16:22 +0700

    update code

--- cpp/tx.cpp
+++ cpp/tx.cpp
@@ -3,6 +3,8 @@ using namespace std ;
 
 int main() {
   std::cout << "Hello world" <<endl ;  
-  
+  std::cout << "hello " <<endl ; 
+  std::cout<< "you got it " <<endl ; 
+  std::cout << "just" <<endl ; 
   return  0 ; 
 }
